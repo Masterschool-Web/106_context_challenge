dd684da89808d9f890a7a6b4120b3228ca07974b
Ishan Rastogi <ishanrastogi15@gmail.com>
2022-11-05 12:12:44 +0100

    solution

--- src/LocationComponent.js
+++ src/LocationComponent.js
@@ -1,15 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 // This component displays location from context
 const Location = () => {
-  return (
-    <div>
-      {/* Display user's location from Context */}
-      <h2 className="is-size-4">
-        <strong>Location</strong>: {"Mars"}
-      </h2>
-    </div>
-  );
+    const user = useContext(UserContext);
+
+    return (
+        <div>
+            {/* Display user's location from Context */}
+            <h2 className="is-size-4">
+                <strong>Location</strong>: {user.location}
+            </h2>
+        </div>
+    );
 };
 
 export default Location;
